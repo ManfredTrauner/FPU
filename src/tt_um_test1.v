6f3c68a8ffa8f4da2abee3b7fb5233e70f90eeed
ManfredTrauner <manfred.trauner@mailbox.org>
2023-12-20 10:03:44 +0100

    fpmu 32

--- src/tt_um_test1.v
+++ src/tt_um_test1.v
@@ -14,16 +14,8 @@ module tt_um_test1 (
 
   reg [10:0] operand_a_mant;
   reg [10:0] operand_b_mant;
-//  reg [4:0]  operand_a_exp;  
-//  reg [4:0]  operand_b_exp;
-//  reg [0:0]  operand_a_sign;
-//  reg [0:0]  operand_b_sign;
-//  reg [21:0]  result_mant;     // max number of digits of 11 x 11 bit multiplication is 22 bit  
-//  reg [5:0]   result_exp;      // max number of digits of 5 bit + 5 bit addition is 6 bit
-//  reg [0:0]   result_sign;
   reg [21:0] iresult_mant;
-//  reg [5:0]  iresult_exp;
-//  reg [0:0]  iresult_sign;
+
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
@@ -39,17 +31,6 @@ module tt_um_test1 (
 
  assign operand_a_mant = 11'b00011110000;
  assign operand_b_mant = 11'b11100001111;
- //assign  reg [4:0]  operand_a_exp;  
- // reg [4:0]  operand_b_exp;
- // reg [0:0]  operand_a_sign;
- // reg [0:0]  operand_b_sign;
- assign iresult_mant = 22'b0000000000000000000000;  
- // reg [5:0]   result_exp;      // max number of digits of 5 bit + 5 bit addition is 6 bit
- // reg [0:0]   result_sign;
- // reg [21:0] iresult_mant;
- // reg [5:0]  iresult_exp;
- // reg [0:0]  iresult_sign;
-
 
  
     always @(posedge clk or posedge reset) begin
@@ -57,13 +38,11 @@ module tt_um_test1 (
         if (reset) begin
             second_counter <= 0;
             digit <= 0;
+            iresult_mant <= operand_a_mant * operand_b_mant;
         end 
     end
     
-    // Mantissa multiplication
-  always @(posedge clk) begin
-     iresult_mant <= operand_a_mant * operand_b_mant;
-  end
+ 
 
 
 endmodule
